118abb1186d2d03d87e37c99daa58bb6eac59ffe
Eduardo Gutiérrez Silva <gt.silva.e@gmail.com>
2017-04-13 04:51:43 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# My implementation of WYOOS
+Hand copied implementation of the (WYOOS)[1] tutorials on YouTube.
+
+## Dependencies
+- Virtualbox
+- grub-pc:i386
+- xorriso
+
+## How to run
+1. Clone the project.
+2. Create the iso file by executing `make mykernel.iso`.
+3. Create a virtual machine in virtualbox.
+4. Launch the machine and select the ISO file.
+5. The text "Hello, World", should appear on the screen.
+
+[1]: http://wyoos.org/Sources/index.php
---
+++ kernel.cpp
@@ -0,0 +1,21 @@
+void printf(char* str) {
+    unsigned short* videoMemory = (unsigned short*)0xb8000;
+
+    for(int i = 0; str[i] != '\0'; ++i)
+	videoMemory[i] = (videoMemory[i] & 0xFF00) | str[i];
+}
+
+typedef void (*constructor)();
+extern "C" constructor start_ctors;
+extern "C" constructor end_ctors;
+
+extern "C" void callConstructors() {
+  for(constructor* i = &start_ctors; i != &end_ctors; ++i)
+    (*i)();
+}
+
+extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber) {
+    printf("Hello, World");
+
+    while(1);
+}
